244348be1dc88b5777e37ade52cdf114b9a8976f
mahirkabir92 <mahirkabir92@gmail.com>
2023-10-31 22:27:42 +0500

    Moved the code from folder and made some fixes to make auth work

---
+++ index.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const booksRoute = require("./routes/booksRoute.js");
+const userRoutes = require("./routes/users");
+const cors = require("cors");
+
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URL);
+
+app.use(express.json());
+const SERVERDEVPORT = 4741;
+const CLIENTDEVPORT = 5173;
+app.use(
+  cors({
+    origin: process.env.CLIENT_ORIGIN || `http://localhost:${CLIENTDEVPORT}`,
+  })
+);
+
+app.use("/books", booksRoute);
+app.use("/users", userRoutes);
+
+app.get("/*", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Connected");
+});
+
+const PORT = process.env.PORT || SERVERDEVPORT;
+
+app.listen(PORT, () => {
+  console.log(`App is listening to port: ${PORT}`);
+});
